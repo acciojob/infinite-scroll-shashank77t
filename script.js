c7d89d5f077f28d0a3cc4d64439ec7321dd300ce
shashank77t <110228304+shashank77t@users.noreply.github.com>
2024-05-25 15:55:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //your code here!
+const list=document.getElementById("infi-list");
+function addListItems(count){
+	for(let i=0;i<count;i++){
+		const listItem=document.createElement("li");
+		listItem.textContent=`Item ${list.children.length+1}`;
+		list.appendChild(listItem);
+	}
+}
+addListItems(10);
+list.addEventListener("scroll",function ab(){
+	if(this.scrollHeight-this.scrollTop-this.clientHeight<1){
+		addListItems(2);
+	}
+})
 
